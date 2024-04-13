f1c8eb42c3ce37a6fda1added4459f6e9b83534d
qGlitchyL <rosuirinaanca@gmail.com>
2024-04-13 12:36:00 +0300

    Initial commit

---
+++ Assets/Scripts/SpawningEnemies.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningEnemies : MonoBehaviour
+{
+    public GameObject Enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawning());
+    }
+
+    IEnumerator Spawning()
+    {
+        Vector2 spawnPoint = new Vector2(Random.Range(-8, 8), Random.Range(0.7f, 3));
+        Vector2 spawnPoint1 = new Vector2(Random.Range(-8, 8), Random.Range(0.7f, 3));
+        
+
+        while(true){
+            Instantiate(Enemy, spawnPoint, Quaternion.identity, transform);
+            Instantiate(Enemy, spawnPoint1, Quaternion.identity, transform);
+            
+            yield return new WaitForSeconds(3f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
